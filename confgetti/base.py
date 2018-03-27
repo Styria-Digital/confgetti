d7673ffa110371599cea5fec29bb8276b7fb8783
ntuckovic <nikola.tuckovic@styria.hr>
2018-03-27 14:55:46 +0200

    confgetti base docstringed.

--- confgetti/base.py
+++ confgetti/base.py
@@ -10,14 +10,51 @@ log = logging.getLogger(__name__)
 
 class Confgetti(object):
     def __init__(self, prepare_consul=True, consul_config=None):
+        """
+        Uses passed consul configuration to initalize consul interface,
+        if configuration is passed. In other case, uses default configuration
+        which is defined from environment variables.
+
+        :param prepare_consul: shoud consul client be prepared or no
+        :type prepare_consul: boolean
+        :param consul_config: dictionary holding consul configuration data
+        :type consul_config: dictionary/None
+        """
         if consul_config is not None:
             self.consul = ConsulInterface()
             self.consul.create_connection(consul_config)
         else:
             self.consul = ConsulInterface(prepare_consul)
 
-    def get_variable(self, key, fallback=None, path=None,
-                     use_env=True, use_consul=True):
+    def get_variable(
+            self,
+            key,
+            path=None,
+            fallback=None,
+            use_env=True,
+            use_consul=True):
+        """
+        Gets variable by passed key.
+        It gets variable from environment and assigns it for return.
+        If variable is not found in environment it tries to get variable
+        from Consul service.
+        If variable is not found in environment or in Consul, it returns
+        fallback value.
+
+        :param key: key name of desired variable
+        :type key: string
+        :param path: location of variable on Consul storage.
+        :type path: string/None
+        :param fallback: value that is returned if variable value not found
+        :type fallback: any
+        :param use_env: Should method look into environment for variable or no
+        :type use_env: boolean
+        :param use_consul: Should method look into consul for variable or no
+        :type use_consul: boolean
+
+        :returns: variable value possibly from one source or fallback.
+        :rtype: any
+        """
         variable = None
 
         if use_env is True:
@@ -33,7 +70,7 @@ class Confgetti(object):
                                 self.consul.connection.http.host
                             ))
 
-        # TODO: Handle conversion to wanted type of returned value?
+        # TODO: Handle conversion to wanted type of returned value or treat as string?
         # if variable is not None:
         #     pass
 
