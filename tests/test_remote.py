81ec748b753bb57a95745b64aea593d01cdc76e1
ntuckovic <nikola.tuckovic@styria.hr>
2018-03-27 17:22:35 +0200

    remote tested

--- tests/test_remote.py
+++ tests/test_remote.py
@@ -1,8 +1,30 @@
-from unittest import TestCase
 import os
+import pytest
+import responses
+
 from unittest import TestCase, mock
 
 from confgetti.remote import ConsulInterface
+from confgetti.exceptions import UndefinedConnectionError
+
+
+CONSUL_DUMMY_RESPONSE = {
+    "LockIndex": 0,
+    "Key": "my_variable",
+    "Flags": 0,
+    "Value": "Zm9v",
+    "CreateIndex": 924,
+    "ModifyIndex": 924
+}
+
+CONSUL_DUMMY_RESPONSE_LEVELED = {
+    "LockIndex": 0,
+    "Key": "my_service/my_variable",
+    "Flags": 0,
+    "Value": "Zm9v",
+    "CreateIndex": 924,
+    "ModifyIndex": 924
+}
 
 
 class ConsulInterfaceTestCase(TestCase):
@@ -40,3 +62,71 @@ class ConsulInterfaceTestCase(TestCase):
         assert connection.http.scheme == dummy_consul_config.get('scheme')
         assert connection.token == dummy_consul_config.get('token')
         assert connection.dc == dummy_consul_config.get('dc')
+
+    def test_create_connection_return_exiting(self):
+        dummy_consul_config = {
+            'host': 'bar',
+            'port': 8600,
+            'scheme': 'http',
+            'token': None,
+            'dc': 'mydc'
+        }
+        cgtti = ConsulInterface(prepare_connection=True)
+
+        connection = cgtti.create_connection(dummy_consul_config)
+
+        assert connection.http.host != dummy_consul_config.get('host')
+        assert connection.http.port != dummy_consul_config.get('port')
+        assert connection.dc != dummy_consul_config.get('dc')
+
+    def test__check_connection__error_raises(self):
+        cgtti = ConsulInterface()
+
+        with pytest.raises(UndefinedConnectionError) as excinfo:
+            cgtti._check_connection()
+
+        assert str(excinfo.value) == 'Consul connection is not defined!'
+
+    @responses.activate
+    def test_get_raw_value(self):
+        responses.add(
+            responses.GET,
+            'http://consul:8500/v1/kv/my_variable',
+            json=[CONSUL_DUMMY_RESPONSE],
+            headers={'X-Consul-Index': '924'},
+            status=200
+        )
+
+        cgtti = ConsulInterface(prepare_connection=True)
+
+        assert cgtti.get_raw_value('my_variable') == b'foo'
+
+    @responses.activate
+    def test_get_raw_value_from_path(self):
+        responses.add(
+            responses.GET,
+            'http://consul:8500/v1/kv/my_service/my_variable',
+            json=[CONSUL_DUMMY_RESPONSE_LEVELED],
+            headers={'X-Consul-Index': '924'},
+            status=200
+        )
+
+        cgtti = ConsulInterface(prepare_connection=True)
+
+        assert cgtti.get_raw_value('my_variable', path='my_service') == b'foo'
+
+    @responses.activate
+    def test_get_raw_value_key_does_not_exist(self):
+        responses.add(
+            responses.GET,
+            'http://consul:8500/v1/kv/my_variable',
+            json=[],
+            headers={'X-Consul-Index': '924'},
+            status=404
+        )
+
+        cgtti = ConsulInterface(prepare_connection=True)
+
+        assert cgtti.get_raw_value('my_variable') is None
+
+        
